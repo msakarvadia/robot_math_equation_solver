77c745d0355f25d19b09a89b9eedc382ca144e8a
chrjor <90344922+chrjor@users.noreply.github.com>
2023-11-15 07:58:29 -0600

    added debug world

---
+++ scripts/moveit_debug.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import rospy
+import moveit_commander
+
+
+class Debug:
+    def __init__(self):
+        rospy.init_node("moveit_debug")
+        print("<<>>")
+        self.move_group_arm = moveit_commander.MoveGroupCommander("arm")
+        self.move_group_gripper = moveit_commander.MoveGroupCommander("gripper")
+
+
+    def run(self):
+        rate = rospy.Rate(.1)
+
+        while not rospy.is_shutdown():
+            print("ARM", self.move_group_arm.get_current_pose().pose.position)
+            rate.sleep()
+
+        rospy.spin()
+
+
+if __name__ == "__main__":
+    d = Debug()
+    d.run()
+
