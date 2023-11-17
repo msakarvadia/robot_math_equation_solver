fcc9a4c5f0177378f1ca7f78387681e32fb4e154
Atticus Yang <ayang5@uchicago.edu>
2023-11-17 15:58:10 -0600

    fixed rectangle logic, working on overlap checking

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import torch
 import cv2
 from nn import Net
 import numpy as np
+from cv2 import Rect
 
 network = Net()
 PATH = 'results/model.pth'
@@ -11,55 +12,81 @@ network.eval()
 #need to use CV2 to break test.jpeg into smaller images based on 
 #individal digits bounding boxes
 
-def preview_processing(img):
+class Point2D:
+    def __init__(self, x, y):
+        self.x= x
+        self.y= y
+
+class boundCorners:
+    def __init__(self, x, y, w, h):
+        self.topleft= Point2D(x, y)
+        self.topright= Point2D(x+w, y)
+        self.bottomleft= Point2D(x, y-h)
+        self.bottomright= Point2D(x+w, y-h)
+
+def preview_processing(name, img):
     #opens window to show contours and bounding rectangles to help debug
-    cv2.imshow('preview', img)
+    cv2.imshow(name, img)
 
 def equation_from_image(img):
     #get image binary. first convert image to grayscale.
-    #thresh_binary_inv used because analyzed equation needs to be white with black bg
     #blur image a little to help get rid of noise
     image_extract= cv2.imread(img, cv2.IMREAD_GRAYSCALE)
     image_extract= cv2.blur(image_extract, (5, 5)) #modify kernel value if needed
+
     #modify second variable if binarized image has too much noise from whiteboard
+    #thresh_binary_inv used because analyzed equation needs to be white with black bg
     #black whiteboard marker will work best and guarantee the most success
     image_binary= cv2.threshold(image_extract, 150, 255, cv2.THRESH_BINARY_INV)
 
-    #get contours and rectangular bounds
+    #get contours
+    #image_binary2: image with extracted contours
+    #image_contours: contour data, each contour is a vector of points
+    #hirearchy: hirearchy data of the contours
     #chain_approx_simple will get the endpoints of the lines of the image
-    #try chain_approx_none if it doesn't work well
-    image_binary2, image_contours= cv2.findContours(image_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    #preview_processing(image_contours)
+    #try chain_approx_none if it doesn't work well and we need the full shape
+    image_binary2, image_contours, hirearchy= cv2.findContours(image_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    preview_processing('contour preview', image_binary2)
 
+    #get bounding boxes and coordinates of box corners
+    #takes the image contours and creates bounding boxes for each contour
+    #contour_curves made to approximate shapes in opencv tutorial
+    ##commented out bc might not be needed
+    idx= 0
+    #contour_curves= []
+    bounding_rectangles= []
     bounding_coords= []
-    i= 0
     for cntr in image_contours:
-        image_bounds= cv2.boundingRect(cntr)
-        bounding_coords[i]= image_bounds
-        i += 1
-        preview_processing(image_bounds)
+        #contour_curves[idx]= cv2.approxPolyDP(cntr, 3, True)
+        x, y, w, h= cv2.boundingRect(image_contours[idx])
+        #(x, y) is the top left coordinate of the bounding rectagle
+        bounding_rectangles[idx]= Rect(x, y, w, h)
+        bounding_coords[idx]= boundCorners(x, y, w, h)
+        idx += 1
 
     #check for overlapping rectangles
-    checked_bounds= []
-    i= 0
-    """ #NOTE(MS): I just commented this out so that I can test the rest of the program
+    processed_rectangles= []
+    idx= 0
+    """ #NOTE(MS): I just commented this out so that I can test the rest of the program"""
     #assumed bounding rectangles recognize the numbers from left to right
     #CURRENTLY PSEUDOCODE, NEED TO CHECK STRUCTURE OF BOUNDS
-    for idx in range(0, bounding_coords.len()-1):
-        if (bounding_coords[i].topright.x < bounding_coords[i+1].bottomleft.x
-            or bounding_coords[i].bottomleft.x > bounding_coords[i+1].topright.x
-            or bounding_coords[i].topright.y < bounding_coords[i+1].bottomleft.y
-            or bounding_coords[i].bottomleft.y > bounding_coords[i+1].topright.y):
-            checked_bounds[i]= #TODO: set bounds of this character to be combination of the two
-            idx += 1
-        else:
-            #TODO: set bounds as the original bounds
-            checked_bounds[i]=
-            checked_bounds[i+1]=
+    for idx in range(0, bounding_rectangles.len()-1):
+        #if rectangles overlap, compare sizes, send the larger one to processed_rectangle
+        #skip over the next rectangle, since it was assessed here and determined to overlap
+        if (bounding_coords[idx].topright.x < bounding_coords[idx+1].bottomleft.x
+            or bounding_coords[idx].bottomleft.x > bounding_coords[idx+1].topright.x
+            or bounding_coords[idx].topright.y < bounding_coords[idx+1].bottomleft.y
+            or bounding_coords[idx].bottomleft.y > bounding_coords[idx+1].topright.y):
+            #TODO: compare size of the rectangles, remove the smaller one
             idx += 2
+        
+        #if the two rectangles don't overlap, set processed rectangle as original
+        #second rectangle needs to be checked with the one adjacent to it for overlap
+        else:
+            processed_rectangles[idx]= bounding_rectangles[idx]
+            idx += 1
 
     cv2.resize(img, (28,28))
-    """
     
 #equation_from_image("test.jpeg")
 
