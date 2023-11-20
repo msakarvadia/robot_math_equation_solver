5b8f21ebc3906d667bafd115a8c6aa910e37f925
chrjor <90344922+chrjor@users.noreply.github.com>
2023-11-20 14:00:34 -0600

    debugged cursor service

--- scripts/cursor_service.py
+++ scripts/cursor_service.py
@@ -18,20 +18,28 @@ CHAR_WIDTH = 0.04
 
 
 class SampleWallPoints:
+    """
+    Utility class which samples lidar scans from a given sweep, and converts
+    the data points to cartesian coordinate frame of lidar.
+    """
 
-    # Radians-to-index mappings
+    # Lidar radians-to-index mappings (precomputed)
     lds = np.fromfunction(lambda k: k * np.pi / (180), (360,))
     rp = np.fromfunction(lambda k: ((573.5 + k) % 1147) * np.pi / (573.5), (1147,))
 
     @classmethod
     def lidar_front(cls):
+        """
+        Class method which samples from the sector -Pi/4 to Pi/4.
+        """
+
         # Sample a single lidar scan 
         data = None
         while data is None:
             data = rospy.wait_for_message("/scan", LaserScan, timeout=1)
         ranges = np.array(data.ranges)
 
-        # Adjust for different lidars and only sample from -Pi/4 to Pi/4
+        # Adjust for lidar differences
         if LIDAR == "LDS":
             scan_left = np.array([ranges[i] for i in range(315, 360)])
             scan_right = np.array([ranges[i] for i in range(0, 46)])
@@ -45,7 +53,7 @@ class SampleWallPoints:
             scan_data = np.array([ranges[i] for i in range(430, 717)])
             angles = np.array([cls.rp[i] for i in range(430, 717)])
        
-        # Convert to cartesian coordinates and stack into (x. y) points
+        # Convert coordinates and stack into (x. y) points
         x = np.cos(angles) * scan_data
         y = np.sin(angles) * scan_data
 
@@ -53,14 +61,21 @@ class SampleWallPoints:
 
 
 class WallCursorService:
+    """
+    A ROS service which tracks the current location of the "virtual wall cursor".
+
+    Upon making a "GET" request, clients will receive the current cursor 
+    location and associated transformation matrix that maps 2D character paths to
+    to wall being written on.
+
+    Upon making a "NEXT" request, the service will advance the cursor to the
+    next character and respond with this position.
+    """
 
     def __init__(self):
-
         rospy.init_node("robot_math_wall_cursor_server")
-
         # Start service
         self.segment_service = rospy.Service("/robot_math/wall_cursor_service", Cursor, self.service_response)
-
         # Start cursor locator subscriber
         self.cursor_locator = rospy.Subscriber("/robot_math/cursor_locator", CursorLocate, self.cursor_locator_callback)
 
@@ -76,6 +91,11 @@ class WallCursorService:
 
 
     def reset_cursor(self):
+        """
+        Resets the cursor based on location published by the /robot_math/cursor_locator 
+        topic.
+        """
+
         self.calc_wall_transformation()
 
         # Wait for initialization
@@ -87,20 +107,33 @@ class WallCursorService:
 
 
     def calc_wall_transformation(self):
+        """
+        Method which creates the transformation matrix for mapping 2D characters 
+        onto a vertical wall in front of the robot.
+        """
 
-        # Estimate wall in robot's coordinate frame using a lidar reading
+        # Estimate wall in robot's coordinate frame using RANSAC
         wall_points = SampleWallPoints.lidar_front()
-        model, _ = ransac(wall_points, LineModelND, min_samples=2, max_trials=100, residual_threshold=0.1)
+        model, _ = ransac(wall_points, 
+                          LineModelND, 
+                          min_samples=2, 
+                          max_trials=100, 
+                          residual_threshold=0.1)
         origin, direction_vector = model.params
 
-        # Initialize transformation matrix and the required values
+        # Initialize transformation matrix
         transform = np.zeros((4, 4))
-        theta = math.atan2(-1 * direction_vector[0], direction_vector[1])
-        intercept = (direction_vector[1] / direction_vector[0]) * (-1 * origin[0]) + origin[1] 
-        self.dist_to_wall = abs(intercept) / math.sqrt((direction_vector[1] / direction_vector[0])**2 + 1)
 
-        # Calculate rotation and translation
-        rotation = np.array([[math.cos(theta), -1 * math.sin(theta)],[math.sin(theta), math.cos(theta)]])
+        # Calculate rotation
+        theta = math.atan2(-1 * direction_vector[0], direction_vector[1])
+        rotation = np.array([[math.cos(theta), -1 * math.sin(theta)],
+                             [math.sin(theta), math.cos(theta)]])
+
+        # Calculate translation
+        intercept = ((direction_vector[1] / direction_vector[0]) 
+                     * (-1 * origin[0]) + origin[1])
+        self.dist_to_wall = (abs(intercept) 
+                             / math.sqrt((direction_vector[1] / direction_vector[0])**2 + 1))
         translation = np.array([self.dist_to_wall, 0, 0])
 
         # Build the matrix
@@ -113,14 +146,17 @@ class WallCursorService:
 
 
     def service_response(self, r):
+        """
+        Request callback method for service.
+        """
 
         # Set position on first cursor request or after a movement
         if not self.cursor_set:
             self.reset_cursor()
             self.cursor_set == True
 
-        # Handle request 
-        if r.request == "NEXT":
+        # Handle requests
+        elif r.request == "NEXT":
             self.y_offset += CHAR_WIDTH
 
         return CursorResponse(y_offset=self.y_offset,
@@ -128,9 +164,14 @@ class WallCursorService:
                               transform=self.transform)
 
 
-    def cursor_locator_callback(self, data):
-        # TODO: Get camera angles
-        self.angle_to_cursor = data.image_width - data.cursor_loc 
+    def cursor_locator_callback(self, img):
+        """
+        Callback method which updates the angle to the cursor based on the
+        computer vision node tracking it.
+        """
+
+        # TODO: Get camera field of view angles
+        self.angle_to_cursor = img.image_width - img.cursor_loc 
 
 
 if __name__ == "__main__":
