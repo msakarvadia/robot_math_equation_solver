c2daf77f82ae0673f76692634de7a310c0501dfd
Atticus Yang <ayang5@uchicago.edu>
2023-12-03 15:36:34 -0600

    branch merge

--- scripts/test.py
+++ scripts/test.py
@@ -2,7 +2,12 @@ import torch
 import cv2
 from nn import Net
 import numpy as np
-from cv2 import Rect
+
+import cv_bridge
+import rospy
+from sensor_msgs.msg import Image
+
+#import camra.py
 
 network = Net()
 PATH = 'results/model.pth'
@@ -11,6 +16,7 @@ network.eval()
 
 #need to use CV2 to break test.jpeg into smaller images based on 
 #individal digits bounding boxes
+    
 
 class Point2D:
     def __init__(self, x, y):
@@ -23,76 +29,87 @@ class boundCorners:
         self.topright= Point2D(x+w, y)
         self.bottomleft= Point2D(x, y-h)
         self.bottomright= Point2D(x+w, y-h)
+        self.rectdata= [x, y, w, h]
 
 def preview_processing(name, img):
     #opens window to show contours and bounding rectangles to help debug
     cv2.imshow(name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows() # destroy all windows
 
 def equation_from_image(img):
     #get image binary. first convert image to grayscale.
     #blur image a little to help get rid of noise
     image_extract= cv2.imread(img, cv2.IMREAD_GRAYSCALE)
-    image_extract= cv2.blur(image_extract, (5, 5)) #modify kernel value if needed
+    image_extract= cv2.medianBlur(image_extract, 7)
+    image_extract= cv2.bilateralFilter(image_extract,9,75,75)
+    image_extract= cv2.blur(image_extract, (10, 10)) #modify kernel value if needed
 
     #modify second variable if binarized image has too much noise from whiteboard
     #thresh_binary_inv used because analyzed equation needs to be white with black bg
     #black whiteboard marker will work best and guarantee the most success
-    image_binary= cv2.threshold(image_extract, 150, 255, cv2.THRESH_BINARY_INV)
+    image_binary= cv2.threshold(image_extract, 80, 255, cv2.THRESH_BINARY_INV)
+    image = image_binary[1]
 
     #get contours
-    #image_binary2: image with extracted contours
     #image_contours: contour data, each contour is a vector of points
     #hirearchy: hirearchy data of the contours
     #chain_approx_simple will get the endpoints of the lines of the image
     #try chain_approx_none if it doesn't work well and we need the full shape
-    image_binary2, image_contours, hirearchy= cv2.findContours(image_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    preview_processing('contour preview', image_binary2)
+    image_contours, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    #get bounding boxes and coordinates of box corners
+    idx = 0
+    buffer = 10
+    cropped_images = {}
+    bounding_coords= []
+    for cntr in image_contours:
+       #contour_curves[idx]= cv2.approxPolyDP(cntr, 3, True)
+       x, y, w, h= cv2.boundingRect(image_contours[idx])
+       image_cropped = image[ y-buffer:y+h+buffer, x-buffer:x+w+buffer]
+       if image_cropped.shape[0] != 0 and (image_cropped.shape[0]>= 350 or image_cropped.shape[1]>= 350):
+        bounding_coords.append(boundCorners(x, y, w, h))
+        image_cropped = np.pad(image_cropped, 30)
+        preview_processing("check the image", image_cropped)
+        #reshape each image
+        image_cropped = cv2.resize(image_cropped, (28,28))
+        cropped_images[x] = image_cropped
+
+       idx += 1
+
+    #CODE BELOW INVOLVES CHECKING BOUNDING BOXES FOR OVERLAP.
+    #for efficiency, we will not use it and instead try to write as clear as possible
+    #this code would be important, however, if we would later want the process to work on messier writin
+    """#get bounding boxes and coordinates of box corners
     #takes the image contours and creates bounding boxes for each contour
     #contour_curves made to approximate shapes in opencv tutorial
     ##commented out bc might not be needed
+    distinct_images= []
     idx= 0
-    #contour_curves= []
-    bounding_rectangles= []
-    bounding_coords= []
-    for cntr in image_contours:
-        #contour_curves[idx]= cv2.approxPolyDP(cntr, 3, True)
-        x, y, w, h= cv2.boundingRect(image_contours[idx])
-        #(x, y) is the top left coordinate of the bounding rectagle
-        bounding_rectangles[idx]= Rect(x, y, w, h)
-        bounding_coords[idx]= boundCorners(x, y, w, h)
-        idx += 1
-
-    #check for overlapping rectangles
-    processed_rectangles= []
-    idx= 0
-    """ #NOTE(MS): I just commented this out so that I can test the rest of the program"""
     #assumed bounding rectangles recognize the numbers from left to right
-    #CURRENTLY PSEUDOCODE, NEED TO CHECK STRUCTURE OF BOUNDS
-    for idx in range(0, bounding_rectangles.len()-1):
+    for idx in range(0, len(bounding_coords)-1):
         #if rectangles overlap, compare sizes, send the larger one to processed_rectangle
         #skip over the next rectangle, since it was assessed here and determined to overlap
         if (bounding_coords[idx].topright.x < bounding_coords[idx+1].bottomleft.x
             or bounding_coords[idx].bottomleft.x > bounding_coords[idx+1].topright.x
             or bounding_coords[idx].topright.y < bounding_coords[idx+1].bottomleft.y
             or bounding_coords[idx].bottomleft.y > bounding_coords[idx+1].topright.y):
-            #TODO: compare size of the rectangles, remove the smaller one
+            
+            img= max(cropped_images[idx].shape[0]*cropped_images[idx].shape[1], cropped_images[idx+1].shape[0]*cropped_images[idx+1].shape[1])
+            distinct_images.append(img)
+
             idx += 2
         
         #if the two rectangles don't overlap, set processed rectangle as original
         #second rectangle needs to be checked with the one adjacent to it for overlap
         else:
-            processed_rectangles[idx]= bounding_rectangles[idx]
+            distinct_images.append(cropped_images[idx])
             idx += 1
-
-    cv2.resize(img, (28,28))
+    """
+    return cropped_images
     
-equation_from_image("test.jpeg")
 
 def do_inference(img_name:str, label:int):
     #currently we will just do inference on an image of a single digit
-    #TODO: we should be able to pass in an image as well and classify it (without a file)
     #test_img = "0_46621.jpg"
     test_img = img_name
     img = cv2.imread(test_img,cv2.IMREAD_GRAYSCALE)
@@ -103,7 +120,7 @@ def do_inference(img_name:str, label:int):
     test_img = torch.from_numpy(im_resize.astype(np.single))
     #needs to be shape (1,1,28,28)
     test_img = test_img[None, None, :,:]
-    #print(test_img.shape)
+    print(test_img.shape)
 
     logits = network(test_img)
     #print("logits: ", logits)
@@ -123,3 +140,98 @@ do_inference("0_46621.jpg", 0)
 do_inference("3_40.jpg", 3)
 
 do_inference("8_362.jpg", 8)
+
+
+def process_and_predict_answer_from_cropped_images(cropped_images:dict):
+#We use these keys to find the correct formatting of images
+    keys = list(cropped_images.keys())
+    keys.sort() 
+    print(keys)
+    s=''
+    for i in keys:
+        image = cropped_images[i]
+        image = image.astype(np.float32)
+        image = torch.from_numpy(image)
+        image = image[None, None, :,:]
+        #print(image.shape)
+        logits = network(image)
+        #print("logits: ", logits)
+        #we used NLL to train network so logit w/ lowest score is prediction
+        result = torch.argmax(logits)
+        result= result.item()
+
+        if(result==10):
+            s=s+'-'
+        if(result==11):
+            s=s+'+'
+        if(result==12):
+            s=s+'*'
+        if(result==0):
+            s=s+'0'
+        if(result==1):
+            s=s+'1'
+        if(result==2):
+            s=s+'2'
+        if(result==3):
+            s=s+'3'
+        if(result==4):
+            s=s+'4'
+        if(result==5):
+            s=s+'5'
+        if(result==6):
+            s=s+'6'
+        if(result==7):
+            s=s+'7'
+        if(result==8):
+            s=s+'8'
+        if(result==9):
+            s=s+'9'
+
+        print("prediction: ", result)
+
+    print("string: ", s)
+    s= check_equation(s)
+    answer = eval(s)
+    print("answer: ", answer)
+    return answer
+
+
+def check_equation(eq):
+    print("\n is the equation correct?\n")
+    resp= input("\n press y if so, otherwise input the correct equation with no spaces: \n")
+
+    if resp != "y":
+        eq= resp
+
+    return eq
+
+"""#defining node for receiving images from ros camera
+def img_callback(msg):
+    # converts the incoming ROS message to OpenCV format and HSV 
+    img= bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+    hsv_img= cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    #cropped_images = equation_from_image("test.jpeg")
+    cropped_images = equation_from_image(hsv_img)
+    answer = process_and_predict_answer_from_cropped_images(cropped_images)
+
+
+rospy.init_node('receive_image_from_cam')
+rospy.Subscriber('camera/rgb/image_raw', Image, img_callback)
+#rospy.Publisher('/robot_math/character_path_service', answer, queue_size= 10)
+
+rospy.spin()"""
+
+#processing test images
+cropped_images = equation_from_image("IMG_1686.jpg")
+answer = process_and_predict_answer_from_cropped_images(cropped_images)
+
+"""cropped_images = equation_from_image("IMG_1687.jpg")
+answer = process_and_predict_answer_from_cropped_images(cropped_images)
+
+cropped_images = equation_from_image("IMG_1688.jpeg")
+answer = process_and_predict_answer_from_cropped_images(cropped_images)"""
+
+
+
+#TODO "answer" is what need to be published to the topics that communicates with the arm
